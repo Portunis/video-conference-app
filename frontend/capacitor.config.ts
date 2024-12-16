95ba2c64d6fa418c2fe5dea020148c7be02e5352
koryakov <koryakov@profiterole.group>
2024-12-16 10:31:15 +0700

    ios app

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,7 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.redramkavc.app',
+  // appId: 'com.redramkavc.app',
+  appId: 'com.redramka.redramkaMeet', // ios todo
   appName: 'rrmeet',
   webDir: 'dist',
   server: {
